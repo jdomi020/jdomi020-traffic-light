bdba870224ef21b28a9dcc68a5420433c8b8492c
Jose Dominguez <141463676+jdomi020@users.noreply.github.com>
2023-11-07 04:44:50 +0000

    got the onClick functioning, just need to center everything and add the wire on top

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,17 +5,22 @@ import React, { useState } from "react";
 //create your first component
 const Home = () => {
 	const [ selectedColor, setSelectedColor ] = useState("red");
+	// the react hook, takes the original variable and hooks it up to be more dynamic
+
 	return (
 		<div className="traffic-light">
 			<div
+				onClick={() => setSelectedColor("red")}
 				className={
 					"light red" + ((selectedColor === "red") ? " glow" : "")
 				}></div>
-			<div 
+			<div
+				onClick={() => setSelectedColor("yellow")}
 				className={
 					"light yellow" + ((selectedColor === "yellow") ? " glow" : "")
 				}></div>
 			<div 
+				onClick={() => setSelectedColor("green")}
 				className={
 					"light green" + ((selectedColor === "green") ? " glow" : "")
 				}></div>
